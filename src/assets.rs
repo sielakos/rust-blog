e728a19c83494363b2d69957e06080f3ded007ba
Mariusz Sielski <sielakos@localhost.localdomain>
2021-02-11 14:31:07 +0100

    fix: building assets with overwrite

--- src/assets.rs
+++ src/assets.rs
@@ -3,8 +3,10 @@ use fs_extra::{copy_items, dir};
 use std::path::Path;
 
 pub fn copy_assets<I: AsRef<Path>>(source: &I, target: &I) -> Result<()> {
-    let options = dir::CopyOptions::new();
+    let mut options = dir::CopyOptions::new();
     let from = vec![source];
+
+    options.overwrite = true;
 
     copy_items(&from, target, &options)?;
 
--- src/build.rs
+++ src/build.rs
@@ -1,14 +1,12 @@
 use crate::templates::init_tera;
 use crate::{assets, index, posts};
 use anyhow::Result;
-use fs_extra::dir::remove;
 use std::path::PathBuf;
 
 pub fn build(posts: &PathBuf, target: &PathBuf, assets: &PathBuf, templates: &str) -> Result<()> {
     let tera = init_tera(&templates);
     let posts = posts::read_posts(posts, &tera)?;
 
-    remove(target)?;
     posts::save_posts(&posts, target)?;
     assets::copy_assets(assets, target)?;
     index::build_index(&posts, &tera, target)?;
